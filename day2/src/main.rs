027b0365dc233502c02331a29d2534e769866f56
Nikolaj K. Falsted <nikolajfalsted@gmail.com>
2020-12-02 22:07:37 +0100

    day2 done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,28 +1,46 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
 struct PasswordPolicy {
     letter: char,
-    min: u8,
-    max: u8,
+    x: u8,
+    y: u8,
 }
 
 impl PasswordPolicy {
-    fn new(policy_str: &str) -> Self {
-        let mut split = policy_str.split(" ");
-        let minmax = split.next().unwrap();
-        let letter = split.next().unwrap().chars().next().unwrap();
+    fn new(policy: &str) -> Self {
+        let (minmax, letter) = split2(policy, " ");
+        let (min, max) = split2(minmax, "-");
         PasswordPolicy {
-            letter,
-            min: 0,
-            max: 0,
+            letter: letter.chars().next().unwrap(),
+            x: min.parse().unwrap(),
+            y: max.parse().unwrap(),
         }
     }
+    fn check1(&self, pw: &str) -> bool {
+        let count = pw.chars().filter(|c| c == &self.letter).count();
+        count >= self.x as usize && count <= self.y as usize
+    }
+    fn check2(&self, pw: &str) -> bool {
+        let mut chars = pw.chars();
+        let x = chars.nth(self.x as usize - 1).unwrap_or('_');
+        let y = chars.nth((self.y - self.x) as usize - 1).unwrap_or('_');
+        (x == self.letter && y != self.letter) || (x != self.letter && y == self.letter)
+    }
+}
+
+fn split2<'a>(input: &'a str, pat: &str) -> (&'a str, &'a str) {
+    let mut split = input.splitn(2, pat);
+    (split.next().unwrap(), split.next().unwrap())
 }
 
 fn main() {
     let input = include_str!("input.txt")
-    .lines()
-    .map(|l| {
-        let split = l.splitn(1,":");
-        
-    });
-    println!("Hello, world!");
+        .lines()
+        .filter(|l| {
+            let (pwp, pw) = split2(l, ": ");
+            PasswordPolicy::new(pwp).check2(pw)
+        })
+        .count();
+    println!("{:#?}", input);
 }
